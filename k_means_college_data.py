a2b90299968b48eb18906a1261760808f9171a40
Mahmoud Ramezani <mahmoud.ramezani@uia.no>
2021-04-15 19:35:09 +0200

    Editing the notebook file

--- k_means_college_data.py
+++ k_means_college_data.py
@@ -1,27 +1,22 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Mar  6 19:16:03 2021
-k_means_college_data.py:
     In this script, we try to implement k-means clustering algorithm for the 
     college data. The aim is to find if a college is private or public. Besides, 
     we try to do some explanatory data analyses. 
 @author: mahmoud.ramezani@uia.no
 """
 #%% importing libraries
-
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import confusion_matrix,classification_report
-
-#%% 
+#%% Reading Data
 colleges = pd.read_csv('College_Data',index_col=0)
 colleges.columns
 colleges.head()
 colleges.loc[colleges['Apps']==np.max(colleges['Apps'])]
-
 #%% Data Visualizations
 sns.scatterplot(data=colleges,x='Outstate',y='F.Undergrad',hue='Private')
 sns.scatterplot(data=colleges,x='Room.Board',y='Grad.Rate',hue='Private')
@@ -36,7 +31,6 @@ colleges[colleges['Grad.Rate']>100]['Grad.Rate']
 colleges['Grad.Rate']['Cazenovia College']=100
 g= sns.FacetGrid(colleges,hue='Private',height=4,aspect = 2)
 g = g.map(plt.hist,'Grad.Rate')
-
 #%%  K-means clustering
 model = KMeans(n_clusters=2)
 model.fit(colleges.drop(['Private'],axis=1))
